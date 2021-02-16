a3626211be20d76d829cee7de287c079a552ab11
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2021-02-16 11:39:46 -0800

    feat(client-routes): create client routes
    created CRUD operations for client routes, also hashed seed passwords

--- server/data/seeds/01-client.ts
+++ server/data/seeds/01-client.ts
@@ -2,11 +2,33 @@ import * as Knex from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
-    await knex("admin").del();
+    await knex("client").del();
 
     // Inserts seed entries
-    await knex("admin").insert([
-        { email: 'admin@gmail.com', password: 'password' },
-        { email: 'admin2@gmail.com', password: 'password' },
+    await knex("client").insert([
+        {
+           issue_description: 'there is a tree on the road',
+           confirmation_number: 2323,
+           issue_img: 'www.google.com',
+           issue_level: 'normal',
+           issue_type: 'fallen tree',
+           issue_status: 'pending',
+           times_reported: 1,
+           client_name: 'Lisa',
+           client_email: 'lisa@gmail.com',
+           client_phone: '4342349655'
+        },
+        {
+           issue_description: 'there is a dead animal in my house',
+           confirmation_number: 2323,
+           issue_img: 'www.google.com',
+           issue_level: 'normal',
+           issue_type: 'dead animal',
+           issue_status: 'pending',
+           times_reported: 1,
+           client_name: 'Lisa',
+           client_email: 'lisa@gmail.com',
+           client_phone: '4342349655'
+        },
     ]);
 };
--- server/routes/clientRoutes.ts
+++ server/routes/clientRoutes.ts
@@ -1,6 +1,58 @@
 import express from 'express';
+import { add, getAll, getById, remove, update } from '../models/clientModel';
 
 
 const route = express.Router();
+
+route.get('/issues', async (req, res) => {
+   try {
+      const issues = await getAll();
+      res.status(200).json(issues);
+   } catch (error) {
+      res.status(500).json({ message: error.message });
+   }
+});
+
+route.post('/issue', async (req, res) => {
+   try {
+      const [newIssue] = await add(req.body);
+      res.status(201).json(newIssue);
+   } catch (error) {
+      res.status(500).json({ message: error.message });
+   }
+});
+
+route.get('/issue/:id', async (req, res) => {
+   const id = Number(req.params.id);
+
+   try {
+      const issue = await getById(id);
+      res.status(200).json(issue);
+   } catch (error) {
+      res.status(500).json({ message: error.message });
+   }
+});
+
+route.patch('/update-issue/:id', async (req, res) => {
+   const id = Number(req.params.id);
+
+   try {
+      await update(id, req.body);
+      res.status(200).json({ message: 'Updated' });
+   } catch (error) {
+      res.status(500).json({ message: error.message });
+   }
+});
+
+route.delete('/remove-issue/:id', async (req, res) => {
+   const id = Number(req.params.id);
+
+   try {
+      await remove(id);
+      res.status(200).json({ message: "deleted" });
+   } catch (error) {
+      res.status(500).json({ message: error.message });
+   }
+});
 
 export default route;
